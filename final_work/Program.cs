76bb317be7d1bbca5e2908550642cb8e74823730
Liubov.V.B <griark0611@gmail.com>
2022-12-26 20:41:33 +0300

    add final_work_csharp

---
+++ final_work/Program.cs
@@ -0,0 +1,19 @@
+﻿// Написать программу, которая из имеющегося массива строк
+// формирует массив из строк, длина которых меньше либо равна 
+// три символа.
+
+string text = "hello 2 world :-)";
+Console.WriteLine(text);
+string[] array = text.Split(" ");
+Console.WriteLine();
+string result = String.Empty;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+
+
