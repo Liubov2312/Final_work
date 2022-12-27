48493b233272517a0376ee00e0d3dc8f6f9ca4b6
Liubov.V.B <griark0611@gmail.com>
2022-12-27 11:39:35 +0300

    add changes

--- final_work/Program.cs
+++ final_work/Program.cs
@@ -2,17 +2,33 @@
 // формирует массив из строк, длина которых меньше либо равна 
 // три символа.
 
-string text = "hello 2 world :-)";
-Console.WriteLine(text);
-string[] array = text.Split(" ");
-Console.WriteLine();
-string result = String.Empty;
-for (int i = 0; i < array.Length; i++)
+string[] array = new string[5] {"hello", "2", "hello", "world", ":-)"};
+string[] arr = new string[array.Length];
+
+FillArrayIF(array, arr);
+PrintArray(array);
+PrintArray(arr);
+
+void FillArrayIF(string[] array, string[] arr)
 {
-    if (array[i].Length <= 3)
+    int item = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        arr[item] = array[i];
+        item++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
     {
         Console.Write($"{array[i]} ");
     }
+    Console.WriteLine();
 }
 
 
